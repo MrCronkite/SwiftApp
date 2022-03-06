81d9fb8689fed0450921ba0d8cf75f56fc88a5fa
MrCronkite <vladshima99@gmail.com>
2022-03-06 20:48:30 +0300

    refactoring buttons

--- Timer/Timer/ContentView.swift
+++ Timer/Timer/ContentView.swift
@@ -40,12 +40,12 @@ struct Home : View{
                     
                     Circle()
                         .trim(from:0, to: 1)
-                        .stroke(Color.black.opacity(0.09), style: StrokeStyle(lineWidth: 35, lineCap: .round))
+                        .stroke(Color.black.opacity(0.09), style: StrokeStyle(lineWidth: 15, lineCap: .round))
                         .frame(width: 280, height: 280)
                     
                     Circle()
                         .trim(from:0, to: self.to)
-                        .stroke(Color.red, style: StrokeStyle(lineWidth: 35, lineCap: .round))
+                        .stroke(Color.red, style: StrokeStyle(lineWidth: 15, lineCap: .round))
                         .frame(width: 280, height: 280)
                         .rotationEffect(.init(degrees: -90))
                     
@@ -54,7 +54,7 @@ struct Home : View{
                         Text("\(self.count)")
                             .font(.system(size: 65))
                             .fontWeight(.bold)
-                        Text("Of 15")
+                        Text("Of 30")
                             .font(.title)
                             .padding(.top)
                     }
@@ -64,17 +64,20 @@ struct Home : View{
                    
                     Button(action: {
                         
+                        self.start.toggle()
+                        
                     }){
-                        HStack(spacing: 15){
+                        HStack(spacing: 30){
                             Image(systemName: self.start ? "pause.fill": "play.fill")
                                 .foregroundColor(.white)
                             Text(self.start ? "Pause" : "Play")
                                 .foregroundColor(.white)
                         }
                         .padding(.vertical)
-                        .frame(width: UIScreen.main.bounds.width - 55)
-                        .background(Color.red)
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 50)
+                        .background(Color.purple)
                         .clipShape(Capsule())
+                        .shadow(radius: 10)
                     }
                     
                     Button(action: {
@@ -82,23 +85,36 @@ struct Home : View{
                     }){
                         HStack(spacing: 15){
                             Image(systemName: "arrow.clockwise")
-                                .foregroundColor(.white)
+                                .foregroundColor(.purple)
                             Text("Restart")
-                                .foregroundColor(.white)
+                                .foregroundColor(.purple)
                         }
                         .padding(.vertical)
-                        .frame(width: UIScreen.main.bounds.width - 55)
-                        .background(Color.red)
-                        .clipShape(Capsule())
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 50)
+                        .background(
+                             Capsule()
+                                .stroke(Color.purple, lineWidth: 4)
+                        )
+                        .shadow(radius: 10)
                     }
                     
                 }
+                .padding(.top, 55)
                 
             }
             
         }
         .onReceive(self.time){(_) in
-            print("hello ")
+            
+            if self.start{
+                if self.count != 30{
+                    self.count += 1
+                    print("hell")
+                }else{
+                    self.to = 0
+                    self.start.toggle()
+                }
+            }
         }
     }
 }
