36b612dcf7ba251389e05f299e58fd89584fd44f
MrCronkite <vladshima99@gmail.com>
2022-03-13 22:30:43 +0300

    add buttons

--- airPodsConnected/airPodsConnected/ContentView.swift
+++ airPodsConnected/airPodsConnected/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Home: View{
+    var body: some View{
+          
+        NavigationView{
+            
+            List{
+                ForEach(1...30,id: \.self){index in
+                
+                                           NavigationLink{
+                    Text("Detail Page \(index)")
+                        .navigationBarTitle("Detail")
+                } label: {
+                    Text("Navigation to Page \(index)")
+                }
+                
+            }
+                .navigationTitle("Home")
+                .toolbar {
+                    Button("Show HUD"){
+                        
+                    }
+                }
+            }
+            
+        }
+        
     }
 }
