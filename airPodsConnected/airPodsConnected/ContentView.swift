1e2a739301a1817cabd160bc5233e8984684d82e
MrCronkite <vladshima99@gmail.com>
2022-03-14 21:44:15 +0300

    add buttons

--- airPodsConnected/airPodsConnected/ContentView.swift
+++ airPodsConnected/airPodsConnected/ContentView.swift
@@ -43,8 +43,49 @@ struct Home: View{
                     }
                 }
             }
+            .overlay(HUDView(image: "airpodspro", color: .primary, title: "Connected"))
             
         }
         
+    }
+}
+
+
+extension View{
+    
+    func showHUD(image: String, color: Color = .primary,title: String){
+        
+    }
+}
+
+
+struct HUDView: View{
+    
+    var image: String
+    var color: Color
+    var title: String
+    
+    @Environment(\.colorScheme) var scheme
+    
+    var body: some View{
+        HStack(spacing: 10){
+         
+            Image(systemName: image)
+                .font(.title3)
+                .foregroundColor(color)
+            
+            Text(title)
+                .foregroundColor(.primary)
+        }
+        .padding(.vertical, 10)
+        .padding(.horizontal)
+        .background(
+            scheme == .dark ? Color.black :  Color.white
+        )
+        .clipShape(Capsule())
+        .shadow(color: Color.primary.opacity(0.1), radius: 5, x: 1, y: 5)
+        .shadow(color: Color.primary.opacity(0.03), radius: 5, x: 0, y: -5)
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
