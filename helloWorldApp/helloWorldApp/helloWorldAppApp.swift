4711d3a48061a4c939152592e841a6826bdefdf8
MrCronkite <vladshima99@gmail.com>
2022-05-17 22:32:34 +0300

    new app

---
+++ helloWorldApp/helloWorldApp/helloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldAppApp.swift
+//  helloWorldApp
+//
+//  Created by admin1 on 12.05.22.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
