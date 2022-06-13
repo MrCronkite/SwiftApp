6fd3a8fb791db7b5d240d2b97a3d1554d50dc053
MrCronkite <vladshima99@gmail.com>
2022-06-13 22:36:49 +0300

    alert

--- Task Count/Task Count/ViewController.swift
+++ Task Count/Task Count/ViewController.swift
@@ -9,15 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var labelPass: UILabel!
+    
     override func loadView() {
         super.loadView()
         print("loadView")
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
         hello()
+        
+        buttonOne.layer.cornerRadius = 10
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +41,27 @@ class ViewController: UIViewController {
         print("hi")
     }
     
+    @IBAction func showButton(_ sender: Any) {
+        self.labelPass.text = "Ваш пароль, "
+        self.alert(title: "Внимание", message: "Неправельный пароль", style: .alert )
+       
+    }
+    
+    
+    func alert(title: String, message: String, style: UIAlertController.Style){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+            let text = alertController.textFields?.first
+            self.labelPass.text? += (text?.text)!
+        }
+        alertController.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+
 }
+
 
 
