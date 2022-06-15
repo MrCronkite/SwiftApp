b2971783583ca142fcc53f20d9df892bc6f53339
MrCronkite <vladshima99@gmail.com>
2022-06-15 21:58:03 +0300

    switch

--- Task Count/Task Count/ViewController.swift
+++ Task Count/Task Count/ViewController.swift
@@ -9,38 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var labelPass: UILabel!
     
-    override func loadView() {
-        super.loadView()
-        print("loadView")
-        
-    }
-
+    let mySwitch = UISwitch()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
-        hello()
         
         buttonOne.layer.cornerRadius = 10
+        //buttonOne.setTitle("введи пароль", for: .highlighted)
+        //создание свич
+        self.mySwitch.frame = CGRect(x: 200, y: 400, width: 0, height: 0)
+        self.view.addSubview(self.mySwitch)
+        //self.mySwitch.setOn(true, animated: true)
+        
+        //проверка
+        if self.mySwitch.isOn{
+            print("sw on")
+        }else{
+            print("sw off")
+        }
+        
+        self.mySwitch.addTarget(self, action: #selector(switchChange(paramTarget:)), for: .valueChanged)
+        self.mySwitch.addTarget(self, action: #selector(isOn(target:)), for: .valueChanged)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            print("viewWillApper")
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        print("viewDidLayoutSubviews")
-    }
     
     
-    func hello(){
-        print("hi")
-    }
-    
+    //MARK: Alert
     @IBAction func showButton(_ sender: Any) {
         self.labelPass.text = "Ваш пароль, "
         self.alert(title: "Внимание", message: "Неправельный пароль", style: .alert )
@@ -59,6 +57,25 @@ class ViewController: UIViewController {
         }
         alertController.addAction(action)
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func switchChange(paramTarget: UISwitch){
+        print("param is = ", paramTarget)
+        
+        if paramTarget.isOn{
+            print("sw on")
+        }else{
+            print("sw off")
+        }
+        
+    }
+    
+    @objc func isOn(target: UISwitch){
+        if target.isOn{
+            self.buttonOne.isPointerInteractionEnabled = false
+        }else{
+            self.buttonOne.isPointerInteractionEnabled = true
+        }
     }
 
 }
